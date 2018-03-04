e88dbf7b64d728ed5f88f35a1cdbdb263a9f42a1
Gianluigi Silvestri <>
2018-03-04 11:19:53 +0100

    building visibility graph P22

--- code/P22.py
+++ code/P22.py
@@ -1,5 +1,40 @@
 import numpy as np
 import json
+import pyvisgraph as vg
+import pygame as pg
+import time
+
+def list_to_pygame(list_of_points):
+    pg_list_of_points=[]
+    for point in list_of_points:
+        pg_list_of_points.append(to_pygame(point))
+    return  pg_list_of_points
+
+def set_bg():
+    '''set initial and final position'''
+    screen.fill((255, 255, 255))
+    '''set obstacles'''
+    [pg.draw.polygon(screen, (0, 0, 0), pg_obstacle, 0) for pg_obstacle in pg_obstacles]
+    '''set boundaries'''
+    pg.draw.polygon(screen, (0, 0, 0), pg_bounding_polygon, 1)
+    '''set start and goal positions'''
+
+
+
+
+def to_pygame(coords):
+    '''Convert coordinates into pygame coordinates'''
+    return (int(coords[0] * 14 + width / 2 - 150), int(coords[1] * -14 + height / 2 + 200))
+
+
+'''set pygame env'''
+pg.init()
+infoObject = pg.display.Info()
+screen = pg.display.set_mode((infoObject.current_w, infoObject.current_h))
+width = infoObject.current_w
+height = infoObject.current_h
+background_colour = (255, 255, 255)
+screen.fill(background_colour)
 
 '''import json data'''
 data = json.load(open('P22.json'))
@@ -21,3 +56,44 @@ vehicle_phi_max = data["vehicle_phi_max"]
 vehicle_t = data["vehicle_t"]
 vehicle_v_max = data["vehicle_v_max"]
 
+'''set pygame obstacles anf bounding polygon'''
+pg_obstacles=[]
+for obstacle in obstacles:
+    pg_obstacles.append(list_to_pygame(obstacle))
+
+pg_bounding_polygon=list_to_pygame(bounding_polygon)
+
+'''create visibility graph'''
+vg_obstacles=[]
+for obstacle in obstacles:
+    vg_obstacles.append([vg.Point(p[0],p[1]) for p in obstacle])
+
+g=vg.VisGraph()
+g.build(vg_obstacles)
+
+pg_edges=[]
+
+for edge in g.visgraph.edges:
+    pg_edges.append(list_to_pygame(edge))
+
+
+
+time_step=0
+start = False
+done = False
+
+
+while not done:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            done = True
+    if not start:
+        t0 = time.time()
+        t1 = 0
+        while (t1 - t0 < 1):
+            t1 = time.time()
+        start = True
+
+        set_bg()
+        # set_data(total_time * 0.1)
+        pg.display.flip()
