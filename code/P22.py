744c8f815a98bf3fca647ec1fe5571bf5e4f257f
Alex Hermansson <grobgeld@kth.se>
2018-03-16 17:10:20 +0100

    tests

--- code/P22.py
+++ code/P22.py
@@ -22,8 +22,7 @@ class VRP_GA():
         self.population, self.fitness_values, self.best_score, self.best_gene = self.init_population()
         self.best_scores=np.array([self.best_score])
         self.generation_scores=np.array(([self.best_score]))
-
-
+        self.best_max_length = np.inf
 
 
     def genetic_algorithm(self, generations=50, plot = False, epsilon = 0.01, percent_plot = True):
@@ -456,8 +455,8 @@ D_sg = np.load('D_sg.npy')
 
 N = len(points_of_interest) # number of pickup points
 k = len(start_positions) # number of robots
-pop_size = 2000
-generations = 150
+pop_size = 600
+generations = 250
 
 #N = 5# number of pickup points
 #k = 3 # number of robots
@@ -465,14 +464,15 @@ lambd=6
 
 vrp_ga = VRP_GA(N, k, D_pg, D_pp, D_sp, D_sg, pop_size,lambd,goal_positions)
 vrp_ga.genetic_algorithm(generations,True, 0.01)
+longest_dist = vrp_ga.best_score
 plt.plot(vrp_ga.best_scores)
 plt.plot(vrp_ga.generation_scores)
 plt.show()
-print(vrp_ga.best_score)
+print('distance: ', longest_dist) # 47.516, pop: 600, gen: 250
+print('time: ', longest_dist/vehicle_v_max)
 
 
-
-#np.savetxt('bestgene_547.txt',vrp_ga.best_gene,fmt='%i')
+np.savetxt('bestgene.txt',vrp_ga.best_gene,fmt='%i')
 #gene=np.loadtxt('bestgene_max_lenght.txt',dtype=int)
 gene=vrp_ga.best_gene
 paths=vrp_ga.create_travel_list(gene)
