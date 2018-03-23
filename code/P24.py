c4aa100ce49d4f16160e4e640423487ed53f0831
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2018-03-23 10:14:55 +0100

    loaded json P24

--- code/P24.py
+++ code/P24.py
@@ -1,1 +1,23 @@
-import triangle as tr
+#import triangle as tr
+import json
+import numpy as np
+
+data = json.load(open('Ptest.json'))
+bounding_polygon = data["bounding_polygon"]
+goal_positions=np.array(data["goal_positions"])
+start_positions=np.array(data["start_positions"])
+vehicle_L = data["vehicle_L"]
+vehicle_a_max = data["vehicle_a_max"]
+vehicle_omega_max = data["vehicle_omega_max"]
+vehicle_phi_max = data["vehicle_phi_max"]
+vehicle_t = data["vehicle_t"]
+vehicle_v_max = data["vehicle_v_max"]
+vehicle_dt=data["vehicle_dt"]
+sensor_range=data["sensor_range"]
+
+#load obstacles
+obstacles=[]
+#sh_obstacles=[]
+for d in data:
+    if "obstacle" in d:
+        obstacles.append(data[d])
